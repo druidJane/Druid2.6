1fb9f09858cea41433d68c366643f648a1270315
druid <413429011@qq.com>
2017-02-21 09:29:23 +0800

    添加商品实体

--- interface/src/main/java/com/druid/AccountService.java
+++ interface/src/main/java/com/druid/AccountService.java
@@ -1,13 +1,15 @@
 package com.druid;
 
+import com.druid.core.exception.SystemException;
 import com.druid.model.Account;
 
 import java.util.List;
 
 /**
- * Created by druid on 17-2-20.
+ * Created by druid on 28/11/16.
  */
 public interface AccountService {
-    List<Account> queryAccountList(Account account);
+    List<Account> queryAccountList(Account account) throws SystemException;
+
     void testDubbo();
 }
